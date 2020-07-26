0f552f8954688e6ce3d49dbd6cc6b07d7b89b56a
Roy Wang <falin.wang@outlook.com>
2020-07-26 04:23:17 -0400

    Updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-deep-97360",
     user: "RoyWang",
     password: "",
     database: "smart-brain",
