f678556449587d8612974c5ce8366b4db0634fb3
Roy Wang <falin.wang@outlook.com>
2020-07-26 04:25:35 -0400

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-deep-97360",
-    user: "RoyWang",
-    password: "",
-    database: "smart-brain",
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
